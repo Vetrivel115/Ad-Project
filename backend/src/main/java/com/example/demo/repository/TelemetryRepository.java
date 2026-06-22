c3ee51ea981226eba6d2f674482eb41edcfa1a8e
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-22 10:21:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/TelemetryRepository.java
+++ backend/src/main/java/com/example/demo/repository/TelemetryRepository.java
@@ -1,8 +1,7 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TelemetryRepository extends JpaRepository{
+public interface TelemetryRepository {
 
     
 }
