ad9602ef53404eb044e07bbd06e883961b04175d
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 23:00:20 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
+++ backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
@@ -36,5 +36,64 @@ public class MaintenanceLog {
     @Column(nullable = false)
     private Double cost;
 
+    public MaintenanceLog() {
+    }
 
+    public MaintenanceLog(Long id, Vehicle vehicle, SystemUser systemUser, LocalDate serviceDate, String description,
+            Double cost) {
+        this.id = id;
+        this.vehicle = vehicle;
+        this.systemUser = systemUser;
+        this.serviceDate = serviceDate;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public SystemUser getSystemUser() {
+        return systemUser;
+    }
+
+    public void setSystemUser(SystemUser systemUser) {
+        this.systemUser = systemUser;
+    }
+
+    public LocalDate getServiceDate() {
+        return serviceDate;
+    }
+
+    public void setServiceDate(LocalDate serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
 }
