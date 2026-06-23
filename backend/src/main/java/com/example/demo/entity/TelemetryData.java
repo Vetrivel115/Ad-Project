25c8829f730737578a9765de352f2274852c9d45
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 20:15:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/TelemetryData.java
+++ backend/src/main/java/com/example/demo/entity/TelemetryData.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,11 @@ public class TelemetryData {
     @JoinColumn(name = "vehicle_id",nullable= false)
     private Vehicle vehicle;
 
-    
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double 
 
     
 }
