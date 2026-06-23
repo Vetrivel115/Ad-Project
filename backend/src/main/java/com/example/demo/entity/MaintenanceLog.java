99cea1ca277342bcc8938c54349a84ec2c2a4e3e
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 22:39:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
+++ backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
@@ -1,11 +1,14 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "maintenance_logs")
 public class MaintenanceLog {
-    
+
+    @Id
+    @G
     
 }
