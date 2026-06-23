bc3a814edb3b0b7029f58a56e46924a580f21f75
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 23:04:50 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Alert.java
+++ backend/src/main/java/com/example/demo/entity/Alert.java
@@ -2,7 +2,10 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,6 +14,10 @@ public class Alert {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id",nullable = false)
+    private Vehicle vehicle;
 
     
     
