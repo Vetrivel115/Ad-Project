8aea230902649dccdf13677d6859f18f056f9bdc
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:41:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Trip.java
+++ backend/src/main/java/com/example/demo/entity/Trip.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,5 +11,8 @@ import jakarta.persistence.Table;
 public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @ManyToOne
+    private Vehicle vehicle;
 }
