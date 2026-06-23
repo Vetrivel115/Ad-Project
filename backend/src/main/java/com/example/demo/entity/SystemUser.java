d4cb52be4d7d9d984bef37e0c38aa7f8fbdb9008
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 18:59:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SystemUser.java
+++ backend/src/main/java/com/example/demo/entity/SystemUser.java
@@ -20,4 +20,7 @@ public class SystemUser {
 
     @Column(unique = true,nullable = false)
     private String email;
+
+    @Column(nullable=false)
+    private UserRole role;
 }
--- backend/src/main/java/com/example/demo/entity/UserRole.java
+++ backend/src/main/java/com/example/demo/entity/UserRole.java
@@ -1,6 +1,5 @@
 package com.example.demo.entity;
 
 public enum UserRole {
-    FLEET_MANAGER,DISPATCHER,DRIVER_MAINTENANCE
-    
+    FLEET_MANAGER,DISPATCHER,DRIVER_MAINTENANCE_TECH
 }
