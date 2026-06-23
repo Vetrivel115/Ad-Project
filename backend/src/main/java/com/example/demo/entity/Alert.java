13762857097aa9ebb818746d7aa452c18d4a1308
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 23:12:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Alert.java
+++ backend/src/main/java/com/example/demo/entity/Alert.java
@@ -30,6 +30,65 @@ public class Alert {
     private String message;
 
     @Column
-    private Boolean isResolved;
-    
+    private Boolean isResolved = false;
+
+    public Alert() {
+    }
+
+    public Alert(Long id, Vehicle vehicle, String type, String severity, String message, Boolean isResolved) {
+        this.id = id;
+        this.vehicle = vehicle;
+        this.type = type;
+        this.severity = severity;
+        this.message = message;
+        this.isResolved = isResolved;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getIsResolved() {
+        return isResolved;
+    }
+
+    public void setIsResolved(Boolean isResolved) {
+        this.isResolved = isResolved;
+    }
 }
