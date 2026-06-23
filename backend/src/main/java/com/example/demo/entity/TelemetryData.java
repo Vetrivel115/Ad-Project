a77be4b90b1ba4ca4cc376ac8b5956f69779aec2
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 20:30:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/TelemetryData.java
+++ backend/src/main/java/com/example/demo/entity/TelemetryData.java
@@ -55,8 +55,69 @@ public class TelemetryData {
         this.recordedAt = recordedAt;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(Double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public Double getEngineTemp() {
+        return engineTemp;
+    }
+
+    public void setEngineTemp(Double engineTemp) {
+        this.engineTemp = engineTemp;
+    }
+
+    public LocalDateTime getRecordedAt() {
+        return recordedAt;
+    }
+
+    public void setRecordedAt(LocalDateTime recordedAt) {
+        this.recordedAt = recordedAt;
+    }
 
     
 }
