4224366ab81f00a92e9a291f456812a1a8353ded
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:20:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Vehicle.java
+++ backend/src/main/java/com/example/demo/entity/Vehicle.java
@@ -29,6 +29,69 @@ public class Vehicle {
     @Column(nullable = false)
     private VehicleStatus status;
 
-    @Column(name = "")
+    @Column(name = "current_mileage")
     private Double currentMileage;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(Long id, String vin, String licensePlate, String model, VehicleStatus status,
+            Double currentMileage) {
+        this.id = id;
+        this.vin = vin;
+        this.licensePlate = licensePlate;
+        this.model = model;
+        this.status = status;
+        this.currentMileage = currentMileage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public VehicleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VehicleStatus status) {
+        this.status = status;
+    }
+
+    public Double getCurrentMileage() {
+        return currentMileage;
+    }
+
+    public void setCurrentMileage(Double currentMileage) {
+        this.currentMileage = currentMileage;
+    }
+
+    
 }
