ef6b9c11afcbee9caa234322ae140e34fdfa915a
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:05:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SystemUser.java
+++ backend/src/main/java/com/example/demo/entity/SystemUser.java
@@ -2,6 +2,8 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class SystemUser {
 
     @Id
+    @GeneratedValue(GenerationType.AUTO)
     private Long id;
 
     @Column(unique = true,nullable = false)
