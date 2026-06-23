c0b508ace91060c17047c679073f3c740684a143
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:24:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Driver.java
+++ backend/src/main/java/com/example/demo/entity/Driver.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = )
+@Table(name = "drivers")
 public class Driver {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long 
     
 }
