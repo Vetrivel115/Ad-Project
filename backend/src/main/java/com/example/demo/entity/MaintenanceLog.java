b9934046273832ac485ef189ddbd9e54a8bdd9ba
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 22:51:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
+++ backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,5 +23,7 @@ public class MaintenanceLog {
 
     @ManyToOne
     @JoinColumn(name = "user_id",nullable = false)
-    private 
+    private SystemUser systemUser;
+
+    @Column
 }
