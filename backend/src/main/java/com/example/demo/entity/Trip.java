fee728c1e6b0af2ac32c6d90f6154a0a9e03d411
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:39:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Trip.java
+++ backend/src/main/java/com/example/demo/entity/Trip.java
@@ -1,6 +1,14 @@
 package com.example.demo.entity;
 
-public class Trip {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-    
+@Entity
+@Table(name = "trips")
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
 }
