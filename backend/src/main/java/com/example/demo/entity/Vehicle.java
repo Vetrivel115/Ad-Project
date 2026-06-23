67e035e8b68103ce395a91add7ee5704e79b0199
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:09:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Vehicle.java
+++ backend/src/main/java/com/example/demo/entity/Vehicle.java
@@ -1,12 +1,15 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "vehicles")
 public class Vehicle {
 
-    @
+    @Id
+    @Genera
+    private Long id;
     
 }
