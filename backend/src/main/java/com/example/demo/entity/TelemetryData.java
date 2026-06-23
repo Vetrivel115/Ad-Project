26b0ac8cca2e041cae3fa130475640b5881d1c1e
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 20:12:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/TelemetryData.java
+++ backend/src/main/java/com/example/demo/entity/TelemetryData.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -15,7 +16,8 @@ public class TelemetryData {
     private Long id;
 
     @ManyToOne
-    private Vehicle
+    @JoinColumn(nullable= false)
+    private Vehicle vehicle;
 
     
 }
