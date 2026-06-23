5c315706382b1d680e5dc8a1909d48fa533379cd
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 20:06:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/TelemetryData.java
+++ backend/src/main/java/com/example/demo/entity/TelemetryData.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "telemetry_data")
 public class TelemetryData {
+    @Id
+    @GeneratedValue(strategy = GeneratedType.AUTO)
     
+
     
 }
