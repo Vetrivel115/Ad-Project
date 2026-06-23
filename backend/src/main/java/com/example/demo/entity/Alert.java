f8a30832b9677f7bcf45ea392db4f214988e6964
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 23:01:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Alert.java
+++ backend/src/main/java/com/example/demo/entity/Alert.java
@@ -1,6 +1,14 @@
 package com.example.demo.entity;
 
-public class Alert {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "alerts")
+public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     
 }
