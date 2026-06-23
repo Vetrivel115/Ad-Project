a0cfe1b1872e968ce136be62b1a61a360b2928d4
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-23 19:54:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/Trip.java
+++ backend/src/main/java/com/example/demo/entity/Trip.java
@@ -39,4 +39,76 @@ public class Trip {
     private TripStatus status;
 
     private Double distanceCovered;
+
+    public Trip() {
+    }
+
+    public Trip(Long id, Vehicle vehicle, Driver driver, LocalDateTime startTime, LocalDateTime endTime,
+            TripStatus status, Double distanceCovered) {
+        this.id = id;
+        this.vehicle = vehicle;
+        this.driver = driver;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+        this.distanceCovered = distanceCovered;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public TripStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TripStatus status) {
+        this.status = status;
+    }
+
+    public Double getDistanceCovered() {
+        return distanceCovered;
+    }
+
+    public void setDistanceCovered(Double distanceCovered) {
+        this.distanceCovered = distanceCovered;
+    }
+
+    
 }
