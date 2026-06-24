847409900b106969c2fdb16a9bfd9ef1ca4478e4
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:42:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/VehicleService.java
+++ backend/src/main/java/com/example/demo/service/VehicleService.java
@@ -1,19 +1,18 @@
 package com.example.demo.service;
 
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Vehicle;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.VehicleRepository;
 
 @Service
 public class VehicleService {
-    private final VehicleRepository vehicleRepo;
+    private VehicleRepository vehicleRepo;
 
-    public VehicleService() {
+    public VehicleService(){
     }
+
 
     public VehicleService(VehicleRepository vehicleRepo) {
         this.vehicleRepo = vehicleRepo;
