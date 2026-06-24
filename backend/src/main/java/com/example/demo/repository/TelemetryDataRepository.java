c3e1b0504d720008cc0d3664f12149e64998d7db
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:08:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/TelemetryDataRepository.java
+++ backend/src/main/java/com/example/demo/repository/TelemetryDataRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.TelemetryData;
+import com.example.demo.entity.Vehicle;
 
 @Repository
 public interface TelemetryDataRepository extends JpaRepository<TelemetryData,Long>{
-    List<TelemetryData> findByVehicle
+    List<TelemetryData> findByVehicleOrderByRecordedAtDesc(Vehicle vehicle);
+    void deleteByVehicle(Vehicle vehicle)
 
     
 }
