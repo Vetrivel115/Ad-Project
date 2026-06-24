38caf8ca7da38ec73fc22d53e3654430c594a599
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 11:53:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface SystemUserRepository extends Jpa{
+import com.example.demo.entity.SystemUser;
 
-    
+@Repository
+public interface SystemUserRepository extends JpaRepository<SystemUser,Long>{
+    Optional<SystemUser> findByUser
 }
