13ed22b1ed91c548707c59cbc8b4cf99194b8ef1
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:14:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/MaintenanceLogRepository.java
+++ backend/src/main/java/com/example/demo/repository/MaintenanceLogRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MaintenanceLogRepository {
+import com.example.demo.entity.MaintenanceLog;
+import com.example.demo.entity.Vehicle;
 
-    
+@Repository
+public interface MaintenanceLogRepository extends JpaRepository<MaintenanceLog,Long>{
+    void deleteByVehicle(Vehicle vehicle);
 }
