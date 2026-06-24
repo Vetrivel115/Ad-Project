e044039ec0a110c1d21142e000214ecea5552dc8
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:00:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/DriverRepository.java
+++ backend/src/main/java/com/example/demo/repository/DriverRepository.java
@@ -1,6 +1,12 @@
 package com.example.demo.repository;
 
-public interface DriverRepository {
+import java.sql.Driver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver>{
 
     
 }
