72b53fc7b6f66dc5b5e3b8b670039a9e4131e424
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:03:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/TripRepository.java
+++ backend/src/main/java/com/example/demo/repository/TripRepository.java
@@ -1,7 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.List;
 
-public interface TripRepository{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.Trip;
+import com.example.demo.entity.TripStatus;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip,Long>{
+    List<Trip> findAllByStatus(TripStatus status);
+    
     
 }
