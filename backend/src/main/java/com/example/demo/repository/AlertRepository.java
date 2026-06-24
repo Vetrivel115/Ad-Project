acf37cc0d5332479f0fbc0c7e16c72f574415f3b
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 12:17:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/AlertRepository.java
+++ backend/src/main/java/com/example/demo/repository/AlertRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
 
-public interface AlertRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    
+import com.example.demo.entity.Alert;
+import com.example.demo.entity.Vehicle;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert,Long>{
+    List<Alert> findByVehicle(Vehicle vehicle);
+    void deleteByVehicle
 } 
