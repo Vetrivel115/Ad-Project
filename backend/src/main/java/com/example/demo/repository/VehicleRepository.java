1cfdca1b725b1af702a3255ab3d450a4447f1467
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-06-24 11:56:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/VehicleRepository.java
+++ backend/src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,6 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.Vehicle;
+import com.example.demo.entity.VehicleStatus;
+
 @Repository
-public interface VehicleRepository extends J
+public interface VehicleRepository extends JpaRepository<Vehicle,Long>{
+    List<Vehicle> findByStatus(VehicleStatus status);
+}
