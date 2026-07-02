6e19c1c31d16d4d182b419bfc67eba98aa59fd20
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-02 12:10:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/VehicleController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Vehicle;
 import com.example.demo.service.VehicleService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 
 @RestController
