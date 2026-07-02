68ce3ce7d030be3ee10518f1c9e710ac61911b16
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-02 11:58:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/VehicleController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,7 +47,11 @@ public class VehicleController {
 
     @PostMapping
     public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle v){
-        Vehicle vvehicleService.
+        Vehicle vehicle = vehicleService.createVehicle(v);
+        return ResponseEntity.status(201).body(vehicle);
     }
+
+    @PutMapping
+    public 
     
 }
