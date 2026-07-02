dcabf437d57d7b5368a5477a744df484cf9341ab
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-02 11:52:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/VehicleController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +35,9 @@ public class VehicleController {
         return ResponseEntity.status(200).body(vehicles);
     } 
 
-    @GetMapping("{id")
-    public 
+    @GetMapping("{id}")
+    public ResponseEntity<Vehicle> getVehicleByID(@PathVariable Long id){
+        Vehicle vehicle = vehicleService.getVehicleById(id)
+    }
     
 }
