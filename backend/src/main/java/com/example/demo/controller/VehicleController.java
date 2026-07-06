4f667863fc58d61e39c878a7559c98789724cdeb
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-06 09:27:46 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/VehicleController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleController.java
@@ -25,9 +25,6 @@ import com.example.demo.service.VehicleService;
 public class VehicleController {
     private VehicleService vehicleService;
 
-    public VehicleController(VehicleService vehicleService) {
-        this.vehicleService = vehicleService;
-    }
     public VehicleController(VehicleService vehicleService,String v) {
         this.vehicleService = vehicleService;
     }
