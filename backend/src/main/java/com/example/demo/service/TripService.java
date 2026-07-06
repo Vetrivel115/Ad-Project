d20c5bdb150d1b67076a6c19147dd0d4e1cdf9d9
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-06 09:21:46 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/TripService.java
+++ backend/src/main/java/com/example/demo/service/TripService.java
@@ -2,7 +2,14 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.repository.TripRepository;
+
 @Service
 public class TripService {
-    
+    private TripRepository tripRepo;
+
+    public TripService(TripRepository tripRepo) {
+        this.tripRepo = tripRepo;
+    }
+    List
 }
