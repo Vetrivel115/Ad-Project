c59f643b5d45c4ff56c5f4a7628ed9d9afb0d315
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-11 09:24:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/MaintenanceService.java
+++ backend/src/main/java/com/example/demo/service/MaintenanceService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.MaintenanceLogRepository;
+
+@Service
 public class MaintenanceService {
-    
+    private MaintenanceLogRepository maintenanceLogRepo;
+    private Vehicle
 }
