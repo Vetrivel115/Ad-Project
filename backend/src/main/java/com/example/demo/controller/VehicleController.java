8c9f5db54acc788ff9c9ea96f74851224c0fd748
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-11 09:48:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/VehicleController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleController.java
@@ -2,8 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
