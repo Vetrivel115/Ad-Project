a59adb87e594c721550b7190341a5439d4737982
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-11 09:57:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/MonitoringService.java
+++ backend/src/main/java/com/example/demo/service/MonitoringService.java
@@ -1,8 +1,21 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.TelemetryRepository;
+import com.example.demo.repository.VehicleRepository;
 
 @Service
 public class MonitoringService {
+    private VehicleRepository vehicleRepo;
+    private TelemetryRepository telemetryRepo;
+
+    public MonitoringService(VehicleRepository vehicleRepo, TelemetryRepository telemetryRepo) {
+        this.vehicleRepo = vehicleRepo;
+        this.telemetryRepo = telemetryRepo;
+    }
     
+    List<Map<>> 
 }
