c85cb899b410cc89da8b413f6f7db103df40201f
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-11 09:05:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/TripService.java
+++ backend/src/main/java/com/example/demo/service/TripService.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Driver;
+import com.example.demo.entity.DriverStatus;
 import com.example.demo.entity.Trip;
 import com.example.demo.entity.Vehicle;
+import com.example.demo.entity.VehicleStatus;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.DriverRepository;
 import com.example.demo.repository.TripRepository;
@@ -29,11 +31,14 @@ public class TripService {
     public List<Trip> getAllTrips(){
         return tripRepo.findAll();
     }
+
     public Trip startTrip(Long vehicleID,Long driverId){
         
         Vehicle vehicle = vehicleRepo.findById(vehicleID).orElseThrow(()-> new ResourceNotFoundException("Vehicle Not Found"));
         Driver driver = driverRepo.findById(driverId).orElseThrow(()-> new ResourceNotFoundException("Driver Not Found"));
         
-        if(vehicle.getStatus()!=)
+        if(vehicle.getStatus()!=VehicleStatus.AVAILABE || driver.getStatus()!=DriverStatus.AVAILABLE){
+            throw new IllegalStateException("");
+        }
     }
 }
