b9e195216358ab52eb97f52fb9262d6b22492fef
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-13 09:27:19 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/TripController.java
+++ backend/src/main/java/com/example/demo/controller/TripController.java
@@ -1,6 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.TripService;
+
+@RestController
+@RequestMapping("/api/trips")
 public class TripController {
+    private TripService tripService;
+
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
+    }
+
+    @GetMapping
+    
 
     
 }
