0064c63a328884d938b860a19ec0e6546e7451c0
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-13 09:37:49 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/TripController.java
+++ backend/src/main/java/com/example/demo/controller/TripController.java
@@ -29,7 +29,7 @@ public class TripController {
 
     @PostMapping("/start")
     public ResponseEntity<?> startTrip(@RequestBody Trip trip){
-        
+        return ResponseEntity.status(200).body(tripService.startTrip(trip.getId(), trip.getDriver()));
     }
 
     
