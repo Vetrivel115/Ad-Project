72a08f7faa2d3b62a430403e2c243bb9302ea098
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 09:15:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests("/api/auth")
+        .authorizeHttpRequests("/api/auth/**").permitAll()
+        .requestMat
+        
     }
 }
