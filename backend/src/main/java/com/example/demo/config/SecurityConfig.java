d11e22092ab24d2eeb145f9bd191121c6e9c20b5
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 09:18:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,8 +18,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> 
             auth.requestMatchers("/api/auth/**").permitAll()
             .anyRequest().authenticated())
-        .sessionManagement(session -> session.session)
-        
-        
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
 }
