5e135f1a3fc61630d20a95f0496bb0de3d12fa4f
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 09:09:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableMethodSecurity
+public class Securityx`Config {
     
 }
