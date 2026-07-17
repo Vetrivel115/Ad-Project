d8f55cfefbc7997a4f3678064bf7596f09eb5739
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 10:25:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> 
-            auth.requestMatchers("/api/auth/**").permitAll()
+            auth.requestMatchers("/","/api/auth/**").permitAll()
             .anyRequest().authenticated())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
