e361e673c6d2e87acc960b3578b9baa1254e01eb
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 09:48:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/TripService.java
+++ backend/src/main/java/com/example/demo/service/TripService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -50,6 +51,7 @@ public class TripService {
         trip.setDriver(driver);
         trip.setVehicle(vehicle);
         trip.setStatus(TripStatus.ACTIVE);
+        trip.setStartTime(LocalDateTime.now());
         return tripRepo.save(trip);
     }
 
