0fe6a500869905c0d660f409b06307df11c2d524
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-17 09:52:42 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/TripController.java
+++ backend/src/main/java/com/example/demo/controller/TripController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,14 @@ public class TripController {
     @PostMapping("/start")
     public ResponseEntity<?> startTrip(@RequestBody Trip trip){
         return ResponseEntity.status(200).body(tripService.startTrip(trip.getId(), trip.getDriver().getId()));
+    }
+
+     @PutMapping("/{id}/end")
+    public ResponseEntity<Trip> endTrip(@PathVariable Long id,
+            @RequestBody Trip trip) {
+
+        return ResponseEntity.ok(
+                tripService.endTrip(id, trip.getDistanceCovered())
+        );
     }
 }
