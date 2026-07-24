88ff236de3decd3d2e8fd1edcaeb436ff96c0b86
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-24 08:57:23 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,69 +1,42 @@
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
+package com.example.demo.service;
 
-// import com.example.demo.config.JwtUtil;
-// import com.example.demo.dto.AuthRequestDto;
-// import com.example.demo.dto.AuthResponseDto;
-// import com.example.demo.entity.SystemUser;
-// import com.example.demo.entity.UserRole;
-// import com.example.demo.repository.SystemUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
-// @Service
-// public class AuthService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Service;
 
-//     private final AuthenticationManager authenticationManager;
-//     private final SystemUserRepository systemUserRepository;
-//     private final PasswordEncoder passwordEncoder;
-//     private final JwtUtil jwtUtil;
 
-//     public AuthService(AuthenticationManager authenticationManager,
-//                        SystemUserRepository systemUserRepository,
-//                        PasswordEncoder passwordEncoder,
-//                        JwtUtil jwtUtil) {
-//         this.authenticationManager = authenticationManager;
-//         this.systemUserRepository = systemUserRepository;
-//         this.passwordEncoder = passwordEncoder;
-//         this.jwtUtil = jwtUtil;
-//     }
 
-//     public AuthResponseDto authenticate(AuthRequestDto request) {
+@Service
+public class AuthService {
 
-//         authenticationManager.authenticate(
-//                 new UsernamePasswordAuthenticationToken(
-//                         request.getUsername(),
-//                         request.getPassword()));
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-//         SystemUser user = systemUserRepository
-        
-//                 .findByUsername(request.getUsername())
-//                 .orElseThrow(() ->
-//                         new UsernameNotFoundException("User not found"));
-
-//         String token = jwtUtil.generateToken(user);
-
-//         AuthResponseDto response = new AuthResponseDto();
-//         response.setToken(token);
-//         response.setUsername(user.getUsername());
-//         response.setRole(user.getRole().name());
-
-//         return response;
-//     }
-
-//     public void register(String username,
-//                          String password,
-//                          String email,
-//                          UserRole role) {
-
-//         SystemUser user = new SystemUser();
-
-//         user.setUsername(username);
-//         user.setPassword(passwordEncoder.encode(password));
-//         user.setEmail(email);
-//         user.setRole(role);
-
-//         systemUserRepository.save(user);
-//     }
-// }
+    @Autowired
+    private JwtService jwtService;
+    public String login(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getEmail(),
+                                request.getPassword()
+                        ));
+        // SecurityContextHolder.getContext().setAuthentication(authentication);
+        // HttpSession session = httpRequest.getSession(true);
+        // System.out.println("Session ID: " + session.getId());
+        // session.setAttribute(
+        //         HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+        //         SecurityContextHolder.getContext()
+        // );
+        UserDetails us=(UserDetails)authentication.getPrincipal();
+        return jwtService.generateToken(us);
+        // return "Login Successful";
+    }
+}
