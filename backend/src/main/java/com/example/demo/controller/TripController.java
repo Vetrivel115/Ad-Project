98905276b1de2aa7380e46a36e33d2108ee35c99
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-07-30 09:12:57 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/TripController.java
+++ backend/src/main/java/com/example/demo/controller/TripController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Trip;
 import com.example.demo.service.TripService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/trips")
