9b410ed5c1bf798cbebd62d670de3f0b9e1c9467
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-20 09:45:47 +0530

    User commit

---
+++ frontend/src/store.js
@@ -0,0 +1,10 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from './components/store/slices/authSlice';
+// import vehicleReducer from './components/store/slices/vehicleSlice';
+
+// export const store = configureStore({
+//     reducer: {
+//         auth: authReducer,
+//         vehicle: vehicleReducer
+//     }
+// });
