bada1c640cf3d7dc538042499f39390c0211319a
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-20 10:06:12 +0530

    User commit

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -24,4 +24,8 @@ const authSlice = createSlice({
 
 export const { login, logout } = authSlice.actions;
 
-export default authSlice.reducer;
+export default {
+    login,
+    logout,
+    reducer: authSlice.reducer
+};
