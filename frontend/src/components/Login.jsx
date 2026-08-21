51007f3c72439aabc305788ea4df71117aa941d4
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-21 11:28:23 +0530

    User commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,9 +5,10 @@ export default function Login() {
 const [userName,setUserName] = useState("");
 const [password,setPassword] = useState("");
 
+
 return (
 <div>
-    <form>
+    <form onSubmit={handleSubmit}>
         <h1>FleetFocus Login</h1>
 
         <input type="text" placeholder='Enter your username'
