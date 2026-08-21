e3f8f9f02489a320ae68da19aafdf2da7f56992b
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-21 11:38:53 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
-import Login from "./components/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
     <App />
-    <Login/>
   </Provider>
 );
