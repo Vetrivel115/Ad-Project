dc487bd61da3b281c1a5c05e3089eb6139345875
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-21 11:19:23 +0530

    User commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 export default function Login() {
 
 const [userName,setUserName] = useState("");
-const [userName,setPassword] = useState("");
+const [password,setPassword] = useState("");
 
 return (
-<div>Login</div>
+<div>
+    <form>
+        <h1>FleetFocus Login</h1>
+        input
+    </form>
+</div>
 )
 
 }
