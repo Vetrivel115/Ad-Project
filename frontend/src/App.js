13f4f360a7f5bb9ca4257c010088c6234b7a4c4c
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-08-28 12:10:26 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Login />
         <a
           className="App-link"
           href="https://reactjs.org"
