2a4795f5ec09f73de84497ae62893e6b6b9e0105
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-07 08:59:47 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {
+    BrowserRouter,
+    Navigate,
+    Route,
+    Routes
+} from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Login from './components/Login';
+
+import VehicleList from
+    './components/vehicles/VehicleList';
+
+import ProtectedRoute from
+    './components/ProtectedRoute';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>
+                FleetFocus Dashboard
+            </h1>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+
+            <Navbar />
+
+            <Routes>
+
+                <Route
+                    path="/login"
+                    element={
+                        <Login />
+                    }
+                />
+
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/vehicles"
+                    element={
+                        <ProtectedRoute>
+                            <VehicleList />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="*"
+                    element={
+                        <Navigate
+                            to="/"
+                        />
+                    }
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
