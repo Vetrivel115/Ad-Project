8f5dea72f6b7018feaf4b0e3e348f3e1fa61df9b
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-10 00:26:30 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,30 +4,30 @@ const api = axios.create({
     baseURL: 'http://localhost:8080/api'
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
+if (
+    api &&
+    api.interceptors &&
+    api.interceptors.request
+) {
+    api.interceptors.request.use(
+        (config) => {
+            const token =
+                localStorage.getItem('token');
 
-        if (token) {
-            config.headers.Authorization =
-                `Bearer ${token}`;
+            if (token) {
+                config.headers =
+                    config.headers || {};
+
+                config.headers.Authorization =
+                    `Bearer ${token}`;
+            }
+
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
         }
-        return config;
-    }
-);
-
-api.interceptors.response.use(
-    (response) => response,
-
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            localStorage.removeItem('role');
-        }
-
-        return Promise.reject(error);
-    }
-);
+    );
+}
 
 export default api;
