28b4c623955b43761db2c03b2e50160f1e936bc2
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-09 09:50:18 +0530

    User commit

--- frontend/src/services/monitoringService.js
+++ frontend/src/services/monitoringService.js
@@ -2,30 +2,21 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api';
 
-
-const getAuthConfig = () => {
-
-    const token =
-        localStorage.getItem('token');
-
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    };
-};
-
-
 const getLiveFleet = async () => {
+
+    const token = localStorage.getItem('token');
 
     const response = await axios.get(
         `${BASE_URL}/monitoring/live`,
-        getAuthConfig()
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
     );
 
     return response.data;
 };
-
 
 export default {
     getLiveFleet
