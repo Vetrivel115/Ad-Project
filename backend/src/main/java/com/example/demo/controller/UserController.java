c6da9a0928b394bdcef799ff5fd70a1f9be836d9
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-09 14:24:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.SystemUser;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
-    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/technicians")
+    @PreAuthorize("hasRole('FLEET_MANAGER')")
+    public ResponseEntity<List<SystemUser>> getTechnicians() {
+
+        return ResponseEntity.ok(
+            userService.getTechnicians()
+        );
+    }
 }
