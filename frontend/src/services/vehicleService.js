9b933472d39162723a8c863ab20500644b4578b2
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-09 09:36:48 +0530

    User commit

--- frontend/src/services/vehicleService.js
+++ frontend/src/services/vehicleService.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api';
 
-const getHeaders = () => {
-    const token =
-        localStorage.getItem('token');
+const getAuthHeaders = () => {
+    const token = localStorage.getItem('token');
 
     return {
         headers: {
@@ -13,74 +12,61 @@ const getHeaders = () => {
     };
 };
 
-
-const getAll = async (
-    page = 0,
-    size = 10
-) => {
+const getAll = async (page = 0, size = 10) => {
     const response = await axios.get(
         `${BASE_URL}/vehicles?page=${page}&size=${size}`,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
-
 
 const getAvailable = async () => {
     const response = await axios.get(
         `${BASE_URL}/vehicles/available`,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
-
 
 const getById = async (id) => {
     const response = await axios.get(
         `${BASE_URL}/vehicles/${id}`,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
-
 
 const create = async (data) => {
     const response = await axios.post(
         `${BASE_URL}/vehicles`,
         data,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
 
-
-const update = async (
-    id,
-    data
-) => {
+const update = async (id, data) => {
     const response = await axios.put(
         `${BASE_URL}/vehicles/${id}`,
         data,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
-
 
 const remove = async (id) => {
     const response = await axios.delete(
         `${BASE_URL}/vehicles/${id}`,
-        getHeaders()
+        getAuthHeaders()
     );
 
     return response.data;
 };
-
 
 export default {
     getAll,
