99562a6aac3952e2ef59600a6aa9eedab8e38df0
VETRIVEL K <727724eucy115@skcet.ac.in>
2026-09-09 09:41:18 +0530

    User commit

--- frontend/src/services/vehicleService.js
+++ frontend/src/services/vehicleService.js
@@ -5,6 +5,8 @@ const BASE_URL = 'http://localhost:8080/api';
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
 
+    console.log('JWT Token:', token);
+
     return {
         headers: {
             Authorization: `Bearer ${token}`
